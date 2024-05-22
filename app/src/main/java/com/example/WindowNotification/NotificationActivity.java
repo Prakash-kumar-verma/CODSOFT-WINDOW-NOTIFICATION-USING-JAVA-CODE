652710ab1a9b45bedb05a287f1f4e86ef80476dc
Prakash-kumar-verma <143523665+Prakash-kumar-verma@users.noreply.github.com>
2024-05-22 11:29:14 +0530

    This Is Our First Commit

---
+++ app/src/main/java/com/example/WindowNotification/NotificationActivity.java
@@ -0,0 +1,21 @@
+package com.example.WindowNotification;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotificationActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_notification);
+        textView = findViewById(R.id.textViewData);
+        String data = getIntent().getStringExtra("data");
+        textView.setText(data);
+    }
+}
